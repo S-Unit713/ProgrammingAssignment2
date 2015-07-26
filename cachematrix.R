7fdbd5d1f21866d486a6d3ec909070381ee3cd2c
Sunit Shukla <sunitshukla713@gmail.com>
2015-07-26 19:10:46 -0400

    Adding assignment 2 changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## create a random matrix and then pass the matrix as makeCacheMatrix(matrix object)
 
 makeCacheMatrix <- function(x = matrix()) {
 
-  I <- NULL
+  i <- NULL
   set <- function(y){
     x<<-y
-    I<<-NULL
+    i<<-NULL
   }
   get<- function() x
-  Setinverse <- function(inv) I <<- inv
-  Getinverse <- function() I
+  Setinverse <- function(inv) i <<- inv
+  Getinverse <- function() i
   list(set = set, get = get,
        Setinverse = Setinverse,
        Getinverse = Getinverse)
 }
 
 
-## Write a short comment describing this function
+## Run functions as >cacheSolve(makeCacheMatrixObject)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  I <- x$Getinverse()
-  if(!is.null(I)) {
+  tracemem(x)
+  b<-x
+  i <- b$Getinverse()
+  if(!is.null(i)) {
     message("getting cached data")
-    return(I)
+    return(i)
   }
   
   data <- x$get()
-  m <- solve(data, ...)
-  x$Setinverse(m)
-  m
+  i <- solve(data, ...)
+  x$Setinverse(i)
+  save(i,file = "myfile.Rdata")
+  i
 }
